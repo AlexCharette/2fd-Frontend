a3d62c649ac1ceb60dfd22eb96515daa36ff7d0f
Alexander Charette <acharette.wake@gmail.com>
2020-12-20 12:10:05 -0500

    Add web support.

--- lib/screens/home/view/home_page.dart
+++ lib/screens/home/view/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_login/authentication/authentication.dart';
 import 'package:flutter_login/authentication/bloc/authentication_bloc.dart';
-import 'package:flutter_login/localization.dart';
 
 class HomePage extends StatelessWidget {
   static Route route() {
@@ -12,8 +11,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar:
-            AppBar(title: Text(FlutterBlocLocalizations.of(context).appTitle)),
+        appBar: AppBar(title: TextField("Hallo")),
         body: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
