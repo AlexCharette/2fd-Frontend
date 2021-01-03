af8584710b2fc0e2453f6787cffba1fcce247c7a
Alexander Charette <acharette.wake@gmail.com>
2021-01-03 08:21:32 -0500

    Add vem and response repositories to app widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:vem_repository/vem_repository.dart';
+import 'package:vem_response_repository/vem_response_repository.dart';
 import 'package:flutter/widgets.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
@@ -6,15 +8,15 @@ import 'package:regimental_app/config/theme.dart';
 
 import 'app.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(
-      ChangeNotifierProvider<ThemeNotifier>(
-        create: (_) => ThemeNotifier(),
-        child: App(
-          authenticationRepository: AuthenticationRepository(),
-        ),
-      )
-  );
+  runApp(ChangeNotifierProvider<ThemeNotifier>(
+    create: (_) => ThemeNotifier(),
+    child: App(
+      authenticationRepository: AuthenticationRepository(),
+      vemRepository: FirebaseVemRepository(),
+      vemResponseRepository: FirebaseVemResponseRepository(),
+    ),
+  ));
 }
