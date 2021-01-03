1886e5b0971c2fb1cb4214a7b8a272320da3c4ef
Alexander Charette <acharette.wake@gmail.com>
2021-01-03 08:32:21 -0500

    Add bloc observer to main function.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:vem_repository/vem_repository.dart';
 import 'package:vem_response_repository/vem_response_repository.dart';
+import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:regimental_app/blocs/simple_bloc_observer.dart';
 import 'package:regimental_app/config/theme.dart';
 
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = SimpleBlocObserver();
   await Firebase.initializeApp();
   runApp(ChangeNotifierProvider<ThemeNotifier>(
     create: (_) => ThemeNotifier(),
