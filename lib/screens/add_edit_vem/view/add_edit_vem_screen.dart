5498be1add8d496052111102dd474bf71c4ac81a
Alexander Charette <acharette.wake@gmail.com>
2021-01-03 08:20:38 -0500

    Finish prototype of vem add-edit screen.

--- lib/screens/add_edit_vem/view/add_edit_vem_screen.dart
+++ lib/screens/add_edit_vem/view/add_edit_vem_screen.dart
@@ -4,7 +4,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:vem_repository/vem_repository.dart';
 
-typedef OnSave = Function();
+typedef OnSave = Function(
+  String name,
+  Timestamp startDate,
+  Timestamp endDate,
+  Timestamp lockDate,
+  String responseType,
+  String description,
+  int minParticipants,
+  int maxParticipiants,
+);
+
+enum ResponseTypes {
+  Battery,
+  Other,
+}
 
 class AddEditVemScreen extends StatefulWidget {
   Vem vem;
@@ -38,65 +52,107 @@ class _AddEditVemScreenState extends State<AddEditVemScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Form(
-                key: _formKey,
-                child: ListView(
-                  children: [
-                    TextFormField(
-                      initialValue: isEditing ? widget.vem.name : '',
-                      autofocus: !isEditing,
-                      validator: (val) {
-                        return val.trim().isEmpty
-                            ? 'The VEM needs a name'
-                            : null;
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                initialValue: isEditing ? widget.vem.name : '',
+                autofocus: !isEditing,
+                validator: (val) {
+                  return val.trim().isEmpty ? 'The VEM needs a name' : null;
+                },
+                onSaved: (value) => _name = value,
+              ),
+              Row(
+                children: <Widget>[], // TODO add icons that call showDatePicker
+              ),
+              Row(
+                children: <Widget>[
+                  // TODO
+                  // Icon(),
+                  TextFormField(
+                    initialValue:
+                        isEditing ? widget.vem.minParticipants.toString() : '1',
+                    decoration:
+                        new InputDecoration(labelText: 'Min participants'),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    validator: (val) {
+                      return _minParticipants > _maxParticipants
+                          ? 'The maximum needs to be higher than the minimum.'
+                          : null;
+                    },
+                    onSaved: (value) => _minParticipants = int.tryParse(value),
+                  ),
+                  TextFormField(
+                    initialValue:
+                        isEditing ? widget.vem.maxParticipants.toString() : '0',
+                    decoration:
+                        new InputDecoration(labelText: 'Max participants'),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    validator: (val) {
+                      return _minParticipants > _maxParticipants
+                          ? 'The maximum needs to be higher than the minimum.'
+                          : null;
+                    },
+                    onSaved: (value) => _maxParticipants = int.tryParse(value),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  ListTile(
+                    title: Text(ResponseTypes.Battery.toString()),
+                    leading: Radio<String>(
+                      value: ResponseTypes.Battery.toString(),
+                      groupValue: _responseType,
+                      onChanged: (String type) {
+                        _responseType = type.toString();
                       },
-                      onSaved: (value) => _name = value,
                     ),
-                    Row(
-                      children: <
-                          Widget>[], // TODO add icons that call showDatePicker
+                  ),
+                  ListTile(
+                    title: Text(ResponseTypes.Other.toString()),
+                    leading: Radio<String>(
+                      value: ResponseTypes.Other.toString(),
+                      groupValue: _responseType,
+                      onChanged: (String type) =>
+                          _responseType = type.toString(),
                     ),
-                    Row(
-                      children: <Widget>[
-                        // TODO
-                        // Icon(),
-                        TextFormField(
-                          initialValue: isEditing
-                              ? widget.vem.minParticipants.toString()
-                              : '1',
-                          decoration: new InputDecoration(
-                              labelText: 'Min participants'),
-                          keyboardType: TextInputType.number,
-                          inputFormatters: <TextInputFormatter>[
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                          validator: (val) {
-                            return _minParticipants > _maxParticipants
-                                ? 'The maximum needs to be higher than the minimum.'
-                                : null;
-                          },
-                        ),
-                        TextFormField(
-                          initialValue: isEditing
-                              ? widget.vem.maxParticipants.toString()
-                              : '0',
-                          decoration: new InputDecoration(
-                              labelText: 'Max participants'),
-                          keyboardType: TextInputType.number,
-                          inputFormatters: <TextInputFormatter>[
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                          validator: (val) {
-                            return _minParticipants > _maxParticipants
-                                ? 'The maximum needs to be higher than the minimum.'
-                                : null;
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ))));
+                  ),
+                ],
+              ),
+              TextFormField(
+                initialValue: isEditing
+                    ? widget.vem.description
+                    : 'Describe the what the VEM entails and requires.',
+                decoration: new InputDecoration(labelText: 'Description'),
+                onSaved: (value) => _description = value,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: isEditing ? 'Save changes' : 'Publish VEM',
+        child: Icon(isEditing ? Icons.check : Icons.add),
+        onPressed: () {
+          if (_formKey.currentState.validate()) {
+            _formKey.currentState.save();
+            widget.onSave(_name, _startDate, _endDate, _lockDate, _responseType,
+                _description, _minParticipants, _maxParticipants);
+            Navigator.pop(context);
+          }
+        },
+      ),
+    );
   }
 }
