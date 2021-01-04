2fece66f3868f7cebf161ef75e3d0ce62b391358
Alexander Charette <acharette.wake@gmail.com>
2021-01-04 14:17:17 -0500

    Add base mail bloc and add VemList to home.

---
+++ lib/blocs/mail/bloc/mail_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'mail_event.dart';
+part 'mail_state.dart';
+
+// class MailBloc extends Bloc<MailEvent, MailState> {
+//   MailBloc();
+
+//   @override
+//   Stream<MailState> mapEventToState() async* {}
+// }
---
+++ lib/blocs/mail/bloc/mail_event.dart
@@ -0,0 +1,6 @@
+part of 'mail_bloc.dart';
+
+class MailEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
