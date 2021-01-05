6beed0b35e9e5cc4fffbabe46d6bcb9a366fea7a
Alexander Charette <acharette.wake@gmail.com>
2021-01-05 12:18:57 -0500

    Fix web files.

---
+++ web/app.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDNhBmP06EFObFIzI-fr0Bkdfuo2zPsqZw",
+    authDomain: "second-fd-app.firebaseapp.com",
+    projectId: "second-fd-app",
+    storageBucket: "second-fd-app.appspot.com",
+    messagingSenderId: "139843623962",
+    appId: "1:139843623962:web:613d53cfe5d14121f605a3",
+    measurementId: "G-WM108MFQ5R"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
