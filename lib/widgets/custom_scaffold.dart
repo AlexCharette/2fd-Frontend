f4994cb8f4a84d04e12fc96f839c311519cd4d44
Juan Hoyos <juanse.jsh@gmail.com>
2021-01-21 12:00:19 -0500

    added a bool attribute to display the image or not

--- lib/widgets/custom_scaffold.dart
+++ lib/widgets/custom_scaffold.dart
@@ -13,6 +13,7 @@ class CustomScaffold extends StatefulWidget {
   final PageController pageController;
   final String appBarTitle;
   final bool displayBottomAppBar;
+  final bool displayBGImage;
   final int selectedIndex;
 
   CustomScaffold({
@@ -22,6 +23,7 @@ class CustomScaffold extends StatefulWidget {
     this.displayBottomAppBar = true,
     this.selectedIndex = 0,
     this.floatingActionButtons,
+    this.displayBGImage = true
   });
 
   @override
@@ -159,13 +161,15 @@ class _CustomScaffoldState extends State<CustomScaffold> {
                       begin: Alignment.topLeft,
                       end: Alignment.bottomRight,
                       colors: [Colors.red, Colors.blue]),
-                  image: DecorationImage(
+                  image: widget.displayBGImage ? DecorationImage(
                       colorFilter: ColorFilter.mode(
                           Colors.black.withOpacity(0.3), BlendMode.dstATop),
                       // TODO change to local asset or data
                       image: NetworkImage(
                           'https://firebasestorage.googleapis.com/v0/b/second-fd-app.appspot.com/o/assets%2Fimages%2Ftradition.jpg?alt=media&token=cd2f7c47-e7c9-449b-83ee-75ae3faa7782'),
-                      fit: BoxFit.cover)),
+                      fit: BoxFit.cover)
+              : null
+              ),
               child: SafeArea(child: widget.body)),
         ),
         floatingActionButton: widget.floatingActionButtons);
